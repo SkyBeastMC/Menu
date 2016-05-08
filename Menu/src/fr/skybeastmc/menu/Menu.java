2962d8034c6cc6001eec7f049ab8e7dc301fe01c
SkyBeastMC <skybeastmc@gmail.com>
2016-05-08 10:54:23 +0200

    Update Menu.java

--- Menu/src/fr/skybeastmc/menu/Menu.java
+++ Menu/src/fr/skybeastmc/menu/Menu.java
@@ -2,7 +2,7 @@ package fr.skybeastmc.menu;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Menu extends JavaPlugin {
+public class Menu extends JavaPlugin { //SLT
 	
 	public void onEnable() {
 		
