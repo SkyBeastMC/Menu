6a42b650a925718215acfd6b8daa027189db7549
SkyBeast <skybeastmc@gmail.com>
2016-05-08 10:53:12 +0200

    Init

---
+++ Menu/src/fr/skybeastmc/menu/Menu.java
@@ -0,0 +1,14 @@
+package fr.skybeastmc.menu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Menu extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
